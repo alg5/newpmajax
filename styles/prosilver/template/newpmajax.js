f2b1f3f14adbd185269b4170082b998ba9fbc2f9
alg5 <chgk.globus@gmail.com>
2014-11-02 07:49:33 +0200

    upd1

--- styles/prosilver/template/newpmajax.js
+++ styles/prosilver/template/newpmajax.js
@@ -1,36 +1,43 @@
-﻿(function ($) {  
+﻿(function ($) {
 
-    
+
     $().ready(function () {
-
-// 
+        // 
         $('#pmheader-postingbox').find('input[name=add_to]').on('click', function (e) {
-                e.preventDefault();
-//                if ($('#group_list :selected').length >0)
-//               $('#postform').submit();
-                 var path = U_NEWPMAJAX_PATH +  'add_to/0/0/0';
-                console.log($('#username_list').val());
-                 $.ajax({
-		                type: 'POST',
-		                dataType: 'json',
-                       data: "username_list=$('#username_list').val()",
-		                url: path,
-		                success: function(data){
-               alert('3');
-			                //togle_thanks(data);
-		                }
-	                });
-              
-                 });
+            e.preventDefault();
+            var data_to_send = 'username_list=' + $('#username_list').val();
+            if ($('#group_list :selected').length > 0) {
+                data_to_send = data_to_send + '&' + $('#group_list ').serialize();
+            }
+            if ($('input[name^=address_list]').length > 0) {
+                data_to_send = data_to_send + '&' + $('input[name^=address_list]').serialize();
+            }
+            //console.log($('.address_list'));
+            //console.log($('input[name^=address_list]'));
+            var path = U_NEWPMAJAX_PATH + 'add_to/0/0';
+            // alert(data_to_send);
+            $.ajax({
+                type: 'POST',
+                dataType: 'json',
+                data: data_to_send,
+                url: path,
+                success: function (data) {
+                    //alert('3');
+                    console.log(data);
+                    //togle_thanks(data);
+                }
+            });
+
+        });
 
 
 
- }); //$().ready(function ()
+    }); //$().ready(function ()
 
 
 
 
 
-})(jQuery);                                                                 // Avoid conflicts with other libraries
+})(jQuery);                                                                             // Avoid conflicts with other libraries
 
 
